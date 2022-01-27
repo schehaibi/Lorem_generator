d48cba92ccdbf16529cb270a7e93d4d81ec5fa1b
sahbi chehaibi <schehaibi72@gmail.com>
2022-01-27 22:16:27 +0100

    Updates

---
+++ precache-manifest.02732620c1dc7462452721c0c2a93368.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6654a22bd9912078af1394408ecccaf3",
+    "url": "/Lorem_generator_React/index.html"
+  },
+  {
+    "revision": "317f5d5d8e729ec371e8",
+    "url": "/Lorem_generator_React/static/css/main.fb86bce3.chunk.css"
+  },
+  {
+    "revision": "222ae14aa9b77537276e",
+    "url": "/Lorem_generator_React/static/js/2.4f3e8bfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Lorem_generator_React/static/js/2.4f3e8bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "317f5d5d8e729ec371e8",
+    "url": "/Lorem_generator_React/static/js/main.e0c4b20c.chunk.js"
+  },
+  {
+    "revision": "7b50b0bc479c8cbe0eff",
+    "url": "/Lorem_generator_React/static/js/runtime-main.1100bfd6.js"
+  }
+]);
